68a35e3ef5bfce77f3a8b15015713493f6a09729
MBennettLowe <mbennettlowe@gmail.com>
2018-10-10 14:05:19 -0400

    Add files via upload
    Scorekeeper project prior to completion.

---
+++ Scorekeeper Project/scorekeeper.js
@@ -0,0 +1,36 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningscore = 5;
+
+
+
+
+p1Button.addEventListener("click", function(){
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningscore){
+            gameOver = true;
+        }
+       
+        p1Display.textContent =p1Score;
+        
+    }
+   
+});
+
+p2Button.addEventListener("click", function(){
+    console.log("CONNECTED!");
+     if(!gameOver){
+        p2Score++;
+        if(p2Score === winningscore){
+            gameOver = true;
+        }
+       
+    p2Display.textContent =p2Score;
+     }
+});
