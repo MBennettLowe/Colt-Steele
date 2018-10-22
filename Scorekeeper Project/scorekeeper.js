703cf4d9f387238300e876f6a75aa7cc34513259
MBennettLowe <mbennettlowe@gmail.com>
2018-10-22 18:09:21 -0400

    Add files via upload
    Complete project with a little refactoring using the "this" object

--- Scorekeeper Project/scorekeeper.js
+++ Scorekeeper Project/scorekeeper.js
@@ -1,14 +1,16 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.querySelector("#p2");
+var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
 
 //Player one score setup
-
 p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
@@ -25,9 +27,35 @@ p2Button.addEventListener("click", function(){
         if(!gameOver){
         p2Score++;
         if(p2Score === winningScore){
-        p2Display.classList.add("winner");
-        gameOver = true;
+            p2Display.classList.add("winner");
+            gameOver = true;
         }
         p2Display.textContent =p2Score;
      }
 });
+
+//Setting up the Reset 
+resetButton.addEventListener("click", function(){
+    reset();
+   
+});
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+//Setting up the <p>Playing to 7<p>
+numInput.addEventListener("change", function(){
+   winningScoreDisplay.textContent = this.value;
+   winningScore = Number(this.value);
+   reset();
+});
+
+
+
